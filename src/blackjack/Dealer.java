635626f139a371130f885316f2bae4272435a7ff
Sean Hart <hart024@gannon.edu>
2020-02-07 10:05:37 -0500

    Complete dealer class

--- src/blackjack/Dealer.java
+++ src/blackjack/Dealer.java
@@ -5,6 +5,8 @@
  */
 package blackjack;
 
+import java.util.Scanner;
+
 /**
  *
  * @author gubotdev
@@ -12,21 +14,65 @@ package blackjack;
 public class Dealer {
     
     private Hand dealerHand = new Hand();
+    private Player[] myPlayers;
+    private Deck myDeck = new Deck();
+    private Scanner scan = new Scanner(System.in);
     
-    public void dealOpeningHand(){
+    public Dealer(int numOfPlayers){
         
+        initMyPlayers(numOfPlayers);
+        
+        
+    }
+    
+    public void dealOutOpeningHand(){
+        for(int i = 0; i < 2; i++){
+            for(Player currPlayer : myPlayers){
+                //for each Player in myPlayers it puts into currPlayer
+                currPlayer.getMyHand().addCard(myDeck.dealCard());
+            }
+            dealerHand.addCard(myDeck.dealCard());
+        }
     }
     
     public void takePlayerTurns(){
-        
+        for(Player currPlayer : myPlayers){
+            while(currPlayer.getMyHand().getNumOfCards() < 5 &&
+                    currPlayer.getMyHand().getScore() < 21){
+                System.out.println(currPlayer.getName() + "'s Hand");
+                currPlayer.getMyHand().printHand();
+                System.out.println("Wanna Hit? (y/n)");
+                char opt = scan.next().charAt(0);
+                if(opt=='y'){
+                    currPlayer.getMyHand().addCard(myDeck.dealCard());
+                }
+            }
+            currPlayer.getMyHand().printHand();
+        }
     }
     
     public void playOutDealerHand(){
-        
+        while(dealerHand.getScore() < 17){
+            dealerHand.addCard(myDeck.dealCard());
+        }
+        dealerHand.printHand();
     }
     
     public void declareWinners(){
         
+    }
+
+    private void initMyPlayers(int num) {
+        myPlayers = new Player[num];
+        for(int i = 0; i < myPlayers.length; i++){
+            System.out.println("Player " + (i+1) + " what's your name: ");
+            String name = scan.next();
+            if(name.equals("")){
+                myPlayers[i] = new Player(i +1);
+            }else{
+                myPlayers[i] = new Player(name);
+            }
+        }
     }
     
 }
